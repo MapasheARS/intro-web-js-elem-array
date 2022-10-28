98ca6ae66c6b80d6b8c4355a73aab773b138c674
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-10-28 12:05:42 -0500

    Actualizacion, elemento array

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Accediendo a los elementos de un array
 
 // Crea el array colores
-
+let colores = ["rosa", "morado", "azul"];
 // Crea el array nombres
-
+let nombres = ["Armando", "Flor", "Agripino", "Ari"];
 // Crea el array instrumentos
-
+let instrumentos = ["piano", "violin",  "arpa"]
 // Crea la variable segundoNombre
-
+let segundoNombre = nombres[1]
 // Crea la variable tercerColor
-
+let tercerColor = colores[2]
 // Crea la variable primerInstrumento
+let primerInstrumento = instrumentos[0]
